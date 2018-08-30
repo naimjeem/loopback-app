e69bdf17217179078a9fa97f66689d75aa7cb6a6
Naim Jeem <naim36208@gmail.com>
2018-08-31 03:33:10 +0600

    fetch data from api

--- view/src/components/Meetupz.js
+++ view/src/components/Meetupz.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 
 class Meetupz extends Component {
+  constructor() {
+    super();
+    this.state = {
+      meetups: []
+    }
+  }
+ 
+  componentWillMount() {
+    this.getMeetups();
+  }
+  
+  getMeetups() {
+    fetch('http://localhost:3000/api/meetupzs')
+      .then(res => {
+         res.json().then(meetups => {
+          console.log(meetups);
+          this.setState({ meetups: meetups }, () => {
+            console.log(this.state);
+          });
+        });
+      });
+  }
+
   render() {
+    const meetupItems = this.state.meetups.map((meetup, index) => {
+      return (
+        <li className="collection-item">{meetup.name}</li>
+      );
+    });
     return (
       <div>
         <h1>Meetupz</h1>
+        <ul className="collection">
+          {meetupItems}
+        </ul>
       </div>
     );
   }
--- view/src/components/Navbar.js
+++ view/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
@@ -7,13 +8,21 @@ class Navbar extends Component {
         <nav>
           <div className="nav-wrapper blue">
             <a href="#" data-activates="mobile-demo" className="button-collapse show-on-large"><i className="material-icons">menu</i></a>
-            <a href="/" className="brand-logo">Meetupz</a>
+            <a href="/" className="brand-logo center">Meetupz</a>
             
             <ul className="side-nav" id="mobile-demo">        
-              <li className="white"><a href="#" className="waves-effect waves-blue"><i className="material-icons">mail</i>Menu item</a></li>
-              <li className="white"><a href="#" className="waves-effect waves-blue"><i className="material-icons">call</i> Menu item</a></li>
-              <li className="white"><a href="#" className="waves-effect waves-blue"><i className="material-icons">android</i> Menu item</a></li>
-              <li className="white"><a href="#" className="waves-effect waves-blue"><i className="material-icons">dialpad</i> Menu item</a></li>              
+              <li className="white">
+                <Link to="/about" className="waves-effect waves-blue">
+                  <i className="material-icons">question_answer</i>
+                  About
+                </Link>
+              </li>
+              <li className="white">
+                <Link to="/" className="waves-effect waves-blue">
+                  <i className="material-icons">list_alt</i>
+                  Meetups
+                </Link>
+              </li>   
             </ul>
             
           </div>
